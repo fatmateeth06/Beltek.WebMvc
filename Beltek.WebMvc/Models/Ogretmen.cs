7fd5b916704bb35317aff2c6ea128897324420df
GEF-LAB1 <GEF-LAB1@DESKTOP-QQQ4J3J>
2023-12-24 11:33:09 +0300

    Add project files.

---
+++ Beltek.WebMvc/Models/Ogretmen.cs
@@ -0,0 +1,10 @@
+﻿namespace Beltek.WebMvc.Models
+{
+    public class Ogretmen
+    {
+        public int TcKimlik { get; set; }//id yap otomatik primary key ve id yapmayı öğren
+        public string Ad  { get; set; }
+        public string Alan { get; set; }//yeni dbset yeni fluent api en son add-migration-updatedatabase tckimlik primary key id crud işlemleri view ile yap
+                                        ////ad student add teacher..
+    }
+}
---
+++ Beltek.WebMvc/Models/OkulDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Beltek.WebMvc.Models
+{
+    public class OkulDbContext : DbContext
+    {
+        public DbSet<Ogrenci> Ogrenciler { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Initial Catalog=OkulDBMVC;Integrated Security=true;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ogrenci>().ToTable("tblOgrenciler");
+            modelBuilder.Entity<Ogrenci>().Property(o => o.Ad).HasColumnType("varchar").HasMaxLength(20).IsRequired();
+            modelBuilder.Entity<Ogrenci>().Property(o => o.Soyad).HasColumnType("varchar").HasMaxLength(30).IsRequired();
+            modelBuilder.Entity<Ogrenci>().Property(o => o.Numara).HasColumnType("varchar").HasMaxLength(15).IsRequired();//FluentApi
+            modelBuilder.Entity<Ogrenci>().Property(o => o.Bolum).HasColumnType("varchar").HasMaxLength(30).IsRequired();//isrequired null olmasın demek
+        }
+    }
+}
