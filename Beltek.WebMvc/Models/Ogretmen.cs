5858dab405eb030cfe4e64a36bebd4f3c9f3332e
farbi <farbi@LENOVOIDEAPAD>
2023-12-25 16:10:42 +0300

    Ogretmen model changed

--- Beltek.WebMvc/Models/Ogretmen.cs
+++ Beltek.WebMvc/Models/Ogretmen.cs
@@ -4,6 +4,7 @@
     {
         public int TcKimlik { get; set; }//id yap otomatik primary key ve id yapmayı öğren
         public string Ad  { get; set; }
+        public string Soyad { get; set; }
         public string Alan { get; set; }//yeni dbset yeni fluent api en son add-migration-updatedatabase tckimlik primary key id crud işlemleri view ile yap
                                         ////ad student add teacher..
     }
